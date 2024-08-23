7ee125914db0eae2e04ac4fbab3a79cd9219e186
Mohanapriya <148181118+MohanapriyaThirumurugan@users.noreply.github.com>
2024-08-23 07:43:51 +0530

    f

--- src/Models/index.js
+++ src/Models/index.js
@@ -5,8 +5,7 @@ dotenv.config()
 async function connectToDatabase () {
     try {
       await mongoose.connect(process.env.dburl,{
-        useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
       serverSelectionTimeoutMS: 30000,  // Increase the timeout to 30 seconds
       connectTimeoutMS: 30000,  
 
